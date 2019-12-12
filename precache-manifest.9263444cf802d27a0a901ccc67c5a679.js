95c13c8a47fa8ce86480300075b9a3ac1947101f
RAFA-PC\Rafael <rafaelcon96@gmail.com>
2019-12-12 10:26:27 +0200

    Updates

---
+++ precache-manifest.9263444cf802d27a0a901ccc67c5a679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a2eb149cd718727b226f9299660488",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "58bf0aed447574b6a665",
+    "url": "/react-monster/static/css/main.401de7b7.chunk.css"
+  },
+  {
+    "revision": "f845c8302c381650f219",
+    "url": "/react-monster/static/js/2.acf109fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monster/static/js/2.acf109fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58bf0aed447574b6a665",
+    "url": "/react-monster/static/js/main.c91f31da.chunk.js"
+  },
+  {
+    "revision": "c53562b83edf314baa81",
+    "url": "/react-monster/static/js/runtime-main.c498238d.js"
+  }
+]);
